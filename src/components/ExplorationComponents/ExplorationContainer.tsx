7c84c1b97758b3499e9292a763672351297c4414
Marius Laber <marius.laber@sap.com>
2019-04-22 11:50:32 +0200

    Update ExplorationContainer.tsx

--- src/components/ExplorationComponents/ExplorationContainer.tsx
+++ src/components/ExplorationComponents/ExplorationContainer.tsx
@@ -7,7 +7,7 @@ export const ExplorationContainer = () => {
         <div id="ExplorationContainer" className="PostSequenceComponent">
             <div style={{ fontWeight: "bold", fontSize: 20 }}>PostSequence Step 3: Exploration </div>
             <ExplorationChartComponent></ExplorationChartComponent>
-            <div style={{ display: "grid", paddingTop: 10 }}>
+            <div style={{ display: "grid", paddingTop: 10, clear: "both" }}>
                 <button className={"ContinueButton"} style={{ display: "grid", clear: "both" }} onClick={continueToNextStep}>Continue</button>
             </div>
         </div>
